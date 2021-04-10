556a32fbac2291c55f2f999c2dcdb9621ddd28ad
Dmitry Pevnitsky <pandarec13@yandex.ru>
2021-04-11 00:34:16 +0300

    added top of design

--- app/src/main/java/com/example/designfromfigma2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/designfromfigma2/ui/home/HomeFragment.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.designfromfigma2.R
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.fragment_home.view.*
 
 class HomeFragment : Fragment() {
 
@@ -22,10 +23,13 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val spinner = root.spinnerGeo
+        val adapter = ArrayAdapter.createFromResource(requireContext(),R.array.testArrayForSpinner,R.layout.spinner_item)
+        spinner.adapter = adapter
+
+
+
+
         return root
     }
 }
